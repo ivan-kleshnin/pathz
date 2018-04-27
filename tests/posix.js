8059fee1c5352962a9aa07cde16c0d8efb22eedb
Ivan Kleshnin <ivan@paqmind.com>
2018-04-27 09:23:58 +0300

    Update test titles

--- tests/posix.js
+++ tests/posix.js
@@ -3,127 +3,127 @@ import P from "../src/posix"
 
 describe("POSIX", () => {
   describe("dir()", () => {
-    it("gets the dir for a relative file path", () => {
+    it("gets a dir for a relative file path", () => {
       eq(P.dir("foo/bar/baz"), "foo/bar/")
     })
 
-    it("gets the dir for a relative dir path", () => {
+    it("gets a dir for a relative dir path", () => {
       eq(P.dir("foo/bar/baz/"), "foo/bar/baz/")
     })
 
-    it("gets the dir for an absolute file path", () => {
+    it("gets a dir for an absolute file path", () => {
       eq(P.dir("/foo/bar/baz"), "/foo/bar/")
     })
 
-    it("gets the dir for an absolute dir path", () => {
+    it("gets a dir for an absolute dir path", () => {
       eq(P.dir("/foo/bar/baz/"), "/foo/bar/baz/")
     })
   })
 
   describe("splitDirs()", () => {
-    it("gets the dirs for a relative file path", () => {
+    it("gets dirs for a relative file path", () => {
       eq(P.splitDirs("foo/bar/baz"), ["foo", "bar"])
     })
 
-    it("gets the dirs for a relative dir path", () => {
+    it("gets dirs for a relative dir path", () => {
       eq(P.splitDirs("foo/bar/"), ["foo", "bar"])
     })
 
-    it("gets the dirs for an absolute file path", () => {
+    it("gets dirs for an absolute file path", () => {
       eq(P.splitDirs("/foo/bar/baz"), ["foo", "bar"])
     })
 
-    it("gets the dirs for an absolute dir path", () => {
+    it("gets dirs for an absolute dir path", () => {
       eq(P.splitDirs("/foo/bar/"), ["foo", "bar"])
     })
   })
 
   describe("base()", () => {
-    it("gets the base for a relative file path", () => {
+    it("gets a base for a relative file path", () => {
       eq(P.base("foo/bar/baz.txt"), "baz.txt")
     })
 
-    it("gets the base for a relative dir path", () => {
+    it("gets a base for a relative dir path", () => {
       eq(P.base("foo/bar/baz/"), "")
     })
 
-    it("gets the base for an absolute file path", () => {
+    it("gets a base for an absolute file path", () => {
       eq(P.base("/foo/bar/baz.txt"), "baz.txt")
     })
 
-    it("gets the base for an absolute dir path", () => {
+    it("gets a base for an absolute dir path", () => {
       eq(P.base("/foo/bar/"), "")
     })
   })
 
   describe("name()", () => {
-    it("gets the name for a relative file path", () => {
+    it("gets a name for a relative file path", () => {
       eq(P.name("foo/bar/baz.txt"), "baz")
     })
 
-    it("gets the name for a relative dir path", () => {
+    it("gets a name for a relative dir path", () => {
       eq(P.name("foo/bar/baz/"), "")
     })
 
-    it("gets the name for an absolute file path", () => {
+    it("gets a name for an absolute file path", () => {
       eq(P.name("/foo/bar/baz.txt"), "baz")
     })
 
-    it("gets the name for an absolute dir path", () => {
+    it("gets a name for an absolute dir path", () => {
       eq(P.name("/foo/bar/"), "")
     })
   })
 
   describe("ext()", () => {
-    it("gets the ext for a relative file path", () => {
+    it("gets an ext for a relative file path", () => {
       eq(P.ext("foo/bar/baz.txt"), ".txt")
     })
 
-    it("gets the ext for a relative dir path", () => {
+    it("gets an ext for a relative dir path", () => {
       eq(P.ext("foo/bar/baz/"), "")
     })
 
-    it("gets the ext for an absolute file path", () => {
+    it("gets an ext for an absolute file path", () => {
       eq(P.ext("/foo/bar/baz.txt"), ".txt")
     })
 
-    it("gets the ext for an absolute dir path", () => {
+    it("gets an ext for an absolute dir path", () => {
       eq(P.ext("/foo/bar/"), "")
     })
   })
 
   describe("leftDir()", () => {
-    it("gets the leftmost dir for a relative file path", () => {
+    it("gets a leftmost dir for a relative file path", () => {
       eq(P.leftDir("foo/bar/baz"), "foo")
     })
 
-    it("gets the leftmost dir for a relative dir path", () => {
+    it("gets a leftmost dir for a relative dir path", () => {
       eq(P.leftDir("foo/bar/baz/"), "foo")
     })
 
-    it("gets the leftmost dir for an absolute file path", () => {
+    it("gets a leftmost dir for an absolute file path", () => {
       eq(P.leftDir("/foo/bar/baz"), "foo")
     })
 
-    it("gets the leftmost dir for an absolute dir path", () => {
+    it("gets a leftmost dir for an absolute dir path", () => {
       eq(P.leftDir("/foo/bar/baz/"), "foo")
     })
   })
 
   describe("rightDir()", () => {
-    it("gets the righmost dir for a relative file path", () => {
+    it("gets a righmost dir for a relative file path", () => {
       eq(P.rightDir("foo/bar/baz"), "bar")
     })
 
-    it("gets the righmost dir for a relative dir path", () => {
+    it("gets a righmost dir for a relative dir path", () => {
       eq(P.rightDir("foo/bar/baz/"), "baz")
     })
 
-    it("gets the righmost dir for an absolute file path", () => {
+    it("gets a righmost dir for an absolute file path", () => {
       eq(P.rightDir("/foo/bar/baz"), "bar")
     })
 
-    it("gets the righmost dir for an absolute dir path", () => {
+    it("gets a righmost dir for an absolute dir path", () => {
       eq(P.rightDir("/foo/bar/baz/"), "baz")
     })
   })
@@ -185,217 +185,217 @@ describe("POSIX", () => {
   })
 
   describe("addLeftDir()", () => {
-    it("adds the leftmost dir for a relative file path", () => {
+    it("adds a leftmost dir for a relative file path", () => {
       eq(P.addLeftDir("bar", "foo/baz"), "bar/foo/baz")
     })
 
-    it("adds the leftmost dir for a relative dir path", () => {
+    it("adds a leftmost dir for a relative dir path", () => {
       eq(P.addLeftDir("bar", "foo/baz/"), "bar/foo/baz/")
     })
 
-    it("adds the leftmost dir for an absolute file path", () => {
+    it("adds a leftmost dir for an absolute file path", () => {
       eq(P.addLeftDir("bar", "/foo/baz"), "/bar/foo/baz")
     })
 
-    it("adds the leftmost dir for an absolute dir path", () => {
+    it("adds a leftmost dir for an absolute dir path", () => {
       eq(P.addLeftDir("bar", "/foo/baz/"), "/bar/foo/baz/")
     })
   })
 
   describe("addRightDir()", () => {
-    it("adds the rightmost dir for a relative file path", () => {
+    it("adds a rightmost dir for a relative file path", () => {
       eq(P.addRightDir("bar", "foo/baz"), "foo/bar/baz")
     })
 
-    it("adds the rightmost dir for a relative dir path", () => {
+    it("adds a rightmost dir for a relative dir path", () => {
       eq(P.addRightDir("bar", "foo/baz/"), "foo/baz/bar/")
     })
 
-    it("adds the rightmost dir for an absolute file path", () => {
+    it("adds a rightmost dir for an absolute file path", () => {
       eq(P.addRightDir("bar", "/foo/baz"), "/foo/bar/baz")
     })
 
-    it("adds the rightmost dir for an absolute dir path", () => {
+    it("adds a rightmost dir for an absolute dir path", () => {
       eq(P.addRightDir("bar", "/foo/baz/"), "/foo/baz/bar/")
     })
   })
 
   describe("dropLeftDir()", () => {
-    it("drops the leftmost dir for a relative file path", () => {
+    it("drops a leftmost dir for a relative file path", () => {
       eq(P.dropLeftDir("foo/bar/baz"), "bar/baz")
     })
 
-    it("drops the leftmost dir for a relative dir path", () => {
+    it("drops a leftmost dir for a relative dir path", () => {
       eq(P.dropLeftDir("foo/bar/baz/"), "bar/baz/")
     })
 
-    it("drops the leftmost dir for an absolute file path", () => {
+    it("drops a leftmost dir for an absolute file path", () => {
       eq(P.dropLeftDir("/foo/bar/baz"), "/bar/baz")
     })
 
-    it("drops the leftmost dir for an absolute dir path", () => {
+    it("drops a leftmost dir for an absolute dir path", () => {
       eq(P.dropLeftDir("/foo/bar/baz/"), "/bar/baz/")
     })
   })
 
   describe("dropRightDir()", () => {
-    it("drops the rightmost dir for a relative file path", () => {
+    it("drops a rightmost dir for a relative file path", () => {
       eq(P.dropRightDir("foo/bar/baz"), "foo/baz")
     })
 
-    it("drops the rightmost dir for a relative dir path", () => {
+    it("drops a rightmost dir for a relative dir path", () => {
       eq(P.dropRightDir("foo/bar/baz/"), "foo/bar/")
     })
 
-    it("drops the rightmost dir for an absolute file path", () => {
+    it("drops a rightmost dir for an absolute file path", () => {
       eq(P.dropRightDir("/foo/bar/baz"), "/foo/baz")
     })
 
-    it("drops the rightmost dir for an absolute dir path", () => {
+    it("drops a rightmost dir for an absolute dir path", () => {
       eq(P.dropRightDir("/foo/bar/baz/"), "/foo/bar/")
     })
   })
 
   describe("withLeftDir()", () => {
-    it("sets the leftmost dir for a relative file path", () => {
+    it("sets a leftmost dir for a relative file path", () => {
       eq(P.withLeftDir("qux", "foo/bar/baz"), "qux/bar/baz")
     })
 
-    it("sets the leftmost dir for a relative dir path", () => {
+    it("sets a leftmost dir for a relative dir path", () => {
       eq(P.withLeftDir("qux", "foo/bar/baz/"), "qux/bar/baz/")
     })
 
-    it("sets the leftmost dir for an absolute file path", () => {
+    it("sets a leftmost dir for an absolute file path", () => {
       eq(P.withLeftDir("qux", "/foo/bar/baz"), "/qux/bar/baz")
     })
 
-    it("sets the leftmost dir for an absolute dir path", () => {
+    it("sets a leftmost dir for an absolute dir path", () => {
       eq(P.withLeftDir("qux", "/foo/bar/baz/"), "/qux/bar/baz/")
     })
   })
 
   describe("withRightDir()", () => {
-    it("sets the rightmost dir for a relative file path", () => {
+    it("sets a rightmost dir for a relative file path", () => {
       eq(P.withRightDir("qux", "foo/bar/baz"), "foo/qux/baz")
     })
 
-    it("sets the rightmost dir for a relative dir path", () => {
+    it("sets a rightmost dir for a relative dir path", () => {
       eq(P.withRightDir("qux", "foo/bar/baz/"), "foo/bar/qux/")
     })
 
-    it("sets the rightmost dir for an absolute file path", () => {
+    it("sets a rightmost dir for an absolute file path", () => {
       eq(P.withRightDir("qux", "/foo/bar/baz"), "/foo/qux/baz")
     })
 
-    it("sets the rightmost dir for an absolute dir path", () => {
+    it("sets a rightmost dir for an absolute dir path", () => {
       eq(P.withRightDir("qux", "/foo/bar/baz/"), "/foo/bar/qux/")
     })
   })
 
   describe("withDir()", () => {
-    it("sets the dir for a relative file path", () => {
+    it("sets a dir for a relative file path", () => {
       eq(P.withDir("qux", "foo/bar/baz"), "qux/baz")
     })
 
-    it("sets the dir for a relative dir path", () => {
+    it("sets a dir for a relative dir path", () => {
       eq(P.withDir("qux", "foo/bar/baz/"), "qux/")
     })
 
-    it("sets the dir for an absolute file path", () => {
+    it("sets a dir for an absolute file path", () => {
       eq(P.withDir("qux", "/foo/bar/baz"), "/qux/baz")
     })
 
-    it("sets the dir for an absolute dir path", () => {
+    it("sets a dir for an absolute dir path", () => {
       eq(P.withDir("qux", "/foo/bar/baz/"), "/qux/")
     })
   })
 
   describe("withBase()", () => {
-    it("sets the base for a relative file path", () => {
+    it("sets a base for a relative file path", () => {
       eq(P.withBase("qux.js", "foo/bar/baz.txt"), "foo/bar/qux.js")
     })
 
-    it("sets the base for a relative dir path", () => {
+    it("sets a base for a relative dir path", () => {
       eq(P.withBase("qux.js", "foo/bar/baz/"), "foo/bar/baz/qux.js")
     })
 
-    it("sets the base for an absolute file path", () => {
+    it("sets a base for an absolute file path", () => {
       eq(P.withBase("qux.js", "/foo/bar/baz.txt"), "/foo/bar/qux.js")
     })
 
-    it("sets the base for an absolute dir path", () => {
+    it("sets a base for an absolute dir path", () => {
       eq(P.withBase("qux.js", "/foo/bar/baz/"), "/foo/bar/baz/qux.js")
     })
   })
 
   describe("withName()", () => {
-    it("sets the name for a relative file path", () => {
+    it("sets a name for a relative file path", () => {
       eq(P.withName("qux", "foo/bar/baz.txt"), "foo/bar/qux.txt")
     })
 
-    it("sets the name for a relative dir path", () => {
+    it("sets a name for a relative dir path", () => {
       eq(P.withName("qux", "foo/bar/baz/"), "foo/bar/baz/qux")
     })
 
-    it("sets the name for an absolute file path", () => {
+    it("sets a name for an absolute file path", () => {
       eq(P.withName("qux", "/foo/bar/baz.txt"), "/foo/bar/qux.txt")
     })
 
-    it("sets the name for an absolute dir path", () => {
+    it("sets a name for an absolute dir path", () => {
       eq(P.withName("qux", "/foo/bar/baz/"), "/foo/bar/baz/qux")
     })
   })
 
   describe("withExt()", () => {
-    it("sets the ext for a relative file path", () => {
+    it("sets an ext for a relative file path", () => {
       eq(P.withExt(".js", "foo/bar/baz.txt"), "foo/bar/baz.js")
     })
 
-    it("sets the ext for a relative dir path", () => {
+    it("sets an ext for a relative dir path", () => {
       eq(P.withExt(".js", "foo/bar/baz/"), "foo/bar/baz/.js")
     })
 
-    it("sets the ext for an absolute file path", () => {
+    it("sets an ext for an absolute file path", () => {
       eq(P.withExt(".js", "/foo/bar/baz.txt"), "/foo/bar/baz.js")
     })
 
-    it("sets the ext for an absolute dir path", () => {
+    it("sets an ext for an absolute dir path", () => {
       eq(P.withExt(".js", "/foo/bar/baz/"), "/foo/bar/baz/.js")
     })
   })
 
   describe("dropBase()", () => {
-    it("drops the base for a relative file path", () => {
+    it("drops a base for a relative file path", () => {
       eq(P.dropBase("foo/bar/baz.txt"), "foo/bar/")
     })
 
-    it("drops the base for a relative dir path", () => {
+    it("drops a base for a relative dir path", () => {
       eq(P.dropBase("foo/bar/"), "foo/bar/")
     })
 
-    it("drops the base for an absolute file path", () => {
+    it("drops a base for an absolute file path", () => {
       eq(P.dropBase("/foo/bar/baz.txt"), "/foo/bar/")
     })
 
-    it("drops the base for an absolute dir path", () => {
+    it("drops a base for an absolute dir path", () => {
       eq(P.dropBase("/foo/bar/"), "/foo/bar/")
     })
   })
 
   describe("dropExt()", () => {
-    it("drops the ext for a relative file path", () => {
+    it("drops an ext for a relative file path", () => {
       eq(P.dropExt("foo/bar/baz.txt"), "foo/bar/baz")
     })
 
-    it("drops the ext for a relative dir path", () => {
+    it("drops an ext for a relative dir path", () => {
       eq(P.dropExt("foo/bar/baz/"), "foo/bar/baz/")
     })
 
-    it("drops the ext for an absolute file path", () => {
+    it("drops an ext for an absolute file path", () => {
       eq(P.dropExt("/foo/bar/baz.txt"), "/foo/bar/baz")
     })
 
-    it("drops the ext for an absolute dir path", () => {
+    it("drops an ext for an absolute dir path", () => {
       eq(P.dropExt("/foo/bar/baz/"), "/foo/bar/baz/")
     })
   })
